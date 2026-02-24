afb3b21a17bd6f674c200db8d28fae4ef01e5e2d
jpfernandes1 <jp_fernandes1@live.com>
2026-02-24 16:55:36 -0300

    feat: create EmailConsumer class to listen the created events;

---
+++ emailSender/src/main/java/com/jompastech/emailSender/consumer/EmailConsumer.java
@@ -0,0 +1,14 @@
+package com.jompastech.emailSender.consumer;
+
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.stereotype.Component;
+
+@Component
+public class EmailConsumer {
+
+    @RabbitListener
+     public void listenEmailQueue(@Payload String emailMessage){
+         System.out.println("Consuming emails message: " + emailMessage);
+     }
+}
