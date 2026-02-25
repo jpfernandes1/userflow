17fce3b47fa1324bc313aa61dea817ee2a62aed7
jpfernandes1 <jp_fernandes1@live.com>
2026-02-25 14:03:42 -0300

    feat: create UserDTO for creating requests;

---
+++ user/src/main/java/com/jompastech/user/model/dto/UserDTO.java
@@ -0,0 +1,12 @@
+package com.jompastech.user.model.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class UserDTO {
+
+    private String name;
+    private String email;
+}
